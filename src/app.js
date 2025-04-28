2eb92c20bb2b2d68d83eb2d1faadbacfffef1935
MeetPatel-2005 <pmalay694@gmail.com>
2025-04-28 21:19:30 +0530

    done

--- src/app.js
+++ src/app.js
@@ -1,36 +1,12 @@
 
-const express = require("express");
-const app = express();
-require("./db/db");
-const path = require("path");
-
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "../views"));
-
-app.use(express.static(path.join(__dirname, "../public")));
-app.use(express.static(path.join(__dirname, "../public/stylesheets")));
-app.use(express.static(path.join(__dirname, "../public/javascripts")));
-app.use(express.static(path.join(__dirname, "../public/images")));
-app.use(express.static(path.join(__dirname, "../public/Gif")));
-app.use(express.static(path.join(__dirname, "../public/Videos")));
-
-module.exports = { app };
-
 // const express = require("express");
 // const app = express();
 // require("./db/db");
 // const path = require("path");
 
-// // Routers
-// const AuthRouter = require("./routes/AuthRouter"); // <<< add this line
-
-// app.use(express.json()); // <<< necessary for parsing JSON bodies
-// app.use(express.urlencoded({ extended: true })); // <<< necessary for parsing form data
-
 // app.set("view engine", "ejs");
 // app.set("views", path.join(__dirname, "../views"));
 
-// // Static files
 // app.use(express.static(path.join(__dirname, "../public")));
 // app.use(express.static(path.join(__dirname, "../public/stylesheets")));
 // app.use(express.static(path.join(__dirname, "../public/javascripts")));
@@ -38,7 +14,31 @@ module.exports = { app };
 // app.use(express.static(path.join(__dirname, "../public/Gif")));
 // app.use(express.static(path.join(__dirname, "../public/Videos")));
 
-// // Mount your Auth Router
-// app.use("/auth", AuthRouter); // <<< important
-
 // module.exports = { app };
+
+const express = require("express");
+const app = express();
+require("./db/db");
+const path = require("path");
+
+// Routers
+const AuthRouter = require("./routes/AuthRouter"); // <<< add this line
+
+app.use(express.json()); // <<< necessary for parsing JSON bodies
+app.use(express.urlencoded({ extended: true })); // <<< necessary for parsing form data
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "../views"));
+
+// Static files
+app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.static(path.join(__dirname, "../public/stylesheets")));
+app.use(express.static(path.join(__dirname, "../public/javascripts")));
+app.use(express.static(path.join(__dirname, "../public/images")));
+app.use(express.static(path.join(__dirname, "../public/Gif")));
+app.use(express.static(path.join(__dirname, "../public/Videos")));
+
+// Mount your Auth Router
+app.use("/auth", AuthRouter); // <<< important
+
+module.exports = { app };
