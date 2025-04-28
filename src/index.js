3be2ab0d641ede07de208b980c7a496ad63d4d94
MeetPatel-2005 <pmalay694@gmail.com>
2025-04-28 21:15:57 +0530

    done

--- src/index.js
+++ src/index.js
@@ -34,8 +34,8 @@ app.get("/reset-password", (req, res) => {
     res.render("reset.ejs");
 });
 
-// app.listen(PORT, () => {
-//     console.log(`Server is running on port ${PORT}`);
-// });
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 
-module.exports = app;
+// module.exports = app;
